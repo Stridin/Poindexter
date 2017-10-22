cff851be71403b85a3ee079ac6d13fe42401d153
Stridin <32247758+Stridin@users.noreply.github.com>
2017-10-22 22:40:35 +0200

    mom get the camera    ***Programmer **hurr *durr

--- Poindexter/Source.cpp
+++ Poindexter/Source.cpp
@@ -1,6 +1,9 @@
 
 int SumPoindexter( int* p )
 {
+	// NOTE: const int* const p
+	// const int* p means value pointedd at cant be modified
+	// int* const p meant pointer cant be redirected
 	int sum = 0;
 	for ( int* e = p + sizeof( p ); p <= e; p++ )
 	{
@@ -13,6 +16,16 @@ int main()
 {
 	int arr[5] = { 2, 4, 12, 7, 15 };
 	int calc = SumPoindexter( arr );
+
+	// int** pp pointer of a pointer
+	int* p = &arr[0];
+	int** pp = &p;
+
+	**pp = 69;
+
+	*pp = &calc;
+
+	*p = 64;
 
 	return 0;
 }
